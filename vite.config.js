e3636243600fa07ec1faabefac17b8229a35be3d
CHARLESJESU <charlesjmng@gmail.com>
2025-02-26 12:50:01 +0530

    hi da

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base : "/myportfolio"
+  base: "/myportfolio/",
+  build: {
+    outDir: "docs"  // Change output directory to docs
+  }
 })
